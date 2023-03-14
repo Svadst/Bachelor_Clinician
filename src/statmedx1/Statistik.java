e034443eb52a9b8ecc270656365746026723541a
Sophia <svadst20@student.aau.dk>
2023-03-14 10:51:20 +0100

    Har tilføjet statistik og database

---
+++ src/statmedx1/Statistik.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package statmedx1;
+
+/**
+ *
+ * @author sophiahaackvadstrup
+ */
+public class Statistik {
+    
+}
---
+++ src/statmedx1/databaseconnector.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package statmedx1;
+import java.sql.Connection;
+
+
+
+/**
+ *
+ * @author sophiahaackvadstrup
+ */
+public class databaseconnector {
+    
+    
+    
+}
