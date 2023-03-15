b1dc7581b140df3c4a9b16946b1e2816a606af8f
rchemn20 <102520498+rchemn20@users.noreply.github.com>
2023-03-15 13:21:41 +0100

    Merge origin/Version into hej
    Conflicts:
    	nbproject/project.properties

--- src/statmedx1/Startside.java
+++ src/statmedx1/Startside.java
@@ -8,12 +8,12 @@ package statmedx1;
  *
  * @author Rikke
  */
-public class LoginClini extends javax.swing.JFrame {
+public class Startside extends javax.swing.JFrame {
 
     /**
-     * Creates new form LoginClini
+     * Creates new form Startside
      */
-    public LoginClini() {
+    public Startside() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class LoginClini extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(LoginClini.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Startside.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(LoginClini.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Startside.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(LoginClini.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Startside.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(LoginClini.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Startside.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new LoginClini().setVisible(true);
+                new Startside().setVisible(true);
             }
         });
     }
