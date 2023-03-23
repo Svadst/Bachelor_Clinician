29b48286da7758c7f6b7978b52c5106b125d8439
rchemn20 <102520498+rchemn20@users.noreply.github.com>
2023-03-23 08:55:16 +0100

    Startside+login Conflicts: 	nbproject/project.properties

--- src/statmedx1/Startside.java
+++ src/statmedx1/Startside.java
@@ -26,21 +26,90 @@ public class Startside extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        Research = new javax.swing.JButton();
+        ClinicalPractice = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(153, 204, 255));
+
+        jLabel1.setFont(new java.awt.Font("Arial", 1, 36)); // NOI18N
+        jLabel1.setText("StatMedX");
+
+        Research.setBackground(new java.awt.Color(102, 153, 255));
+        Research.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        Research.setText("Research");
+        Research.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ResearchActionPerformed(evt);
+            }
+        });
+
+        ClinicalPractice.setBackground(new java.awt.Color(102, 153, 255));
+        ClinicalPractice.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        ClinicalPractice.setText("Clinical practice");
+        ClinicalPractice.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ClinicalPracticeActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(215, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(ClinicalPractice)
+                        .addGap(171, 171, 171)
+                        .addComponent(Research, javax.swing.GroupLayout.PREFERRED_SIZE, 179, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(221, 221, 221))))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1)
+                .addGap(137, 137, 137)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Research, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(ClinicalPractice, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 218, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void ClinicalPracticeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ClinicalPracticeActionPerformed
+        // TODO add your handling code here:
+            dispose();
+            ClinicalLogin CL = new ClinicalLogin();
+            CL.show();
+    }//GEN-LAST:event_ClinicalPracticeActionPerformed
+
+    private void ResearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ResearchActionPerformed
+        // TODO add your handling code here:
+            dispose();
+            ResearchLogin RL = new ResearchLogin();
+            RL.show();
+    }//GEN-LAST:event_ResearchActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +147,9 @@ public class Startside extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton ClinicalPractice;
+    private javax.swing.JButton Research;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
