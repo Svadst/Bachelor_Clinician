fd86576daee83ce6daaffa68c9e1b12ad90ff0f6
Sophia <svadst20@student.aau.dk>
2023-05-03 17:30:40 +0200

    Clinician

--- src/Clinician/ComparableGroup.java
+++ src/Clinician/ComparableGroup.java
@@ -10,61 +10,55 @@ import java.util.Map;
 import Clinician.Filter_Cl;
 import java.util.ArrayList;
 import java.util.List;
+import Clinician.Visualise_Cl1;
+import Clinician.Patient_Cl;
 
+
+
+import java.util.List;
 
 public class ComparableGroup {
-    private List<Patient_Cl> patientGroup;
+    private List<Patient_Cl> similarPatients;
 
-    public ComparableGroup(List<Patient_Cl> patientGroup) {
-        this.patientGroup = patientGroup;
+    public ComparableGroup(int patientCardId) {
+        DatabaseConnection dbConnection = DatabaseConnection.getInstance();
+        similarPatients = dbConnection.fetchSimilarPatients(patientCardId);
     }
 
-    public void compareWithPatient(Patient_Cl patient) {
-        // Filter patient group based on aetiology, gender, age, height, and weight
-        List<Patient_Cl> filteredGroup = filterPatientGroup(patient.getAetiology(), patient.getGender(), patient.getAge(), patient.getHeight(), patient.getWeight());
-
-        // Filter EE values of the patient group using RQ filter
-        List<ICData_Cl> filteredEE_Group = Filter_Cl.filterEEWithRQ((Map<String, List<Object>>) filteredGroup);
-
-        // Calculate the average EE of the filtered patient group
-        double averageEE = calculateAverageEE(filteredEE_Group);
-
-        // Compare the patient's EE with the average EE of the patient group
-        double deviationPercentage = calculateDeviationPercentage(patient.getEE(), averageEE);
+    public List<Patient_Cl> getSimilarPatients() {
+        return similarPatients;
     }
 
-    private List<Patient_Cl> filterPatientGroup(String aetiology, String gender, int age, double height, double weight) {
-        // Create a new list to store the filtered patients
-        List<Patient_Cl> filteredGroup = new ArrayList<>();
+    public void calculateEEValuePerDay() {
+        // Perform calculations for EE value per day for the similar group
+        for (Patient_Cl patient : similarPatients) {
+            // Get the ICData_Cl instances for the current patient
+            List<ICData_Cl> icDataList = patient.getIcDataList_Cl();
 
-        for (Patient_Cl patient : patientGroup) {
-            // Check if the patient's aetiology, gender, age, height, and weight match the criteria
-            if (patient.getAetiology().equals(aetiology) && patient.getGender().equals(gender)
-                    && Math.abs(patient.getAge() - age) <= 10
-                    && Math.abs(patient.getHeight() - height) <= 10
-                    && Math.abs(patient.getWeight() - weight) <= 10) {
-                filteredGroup.add(patient);
+            // Calculate EE value per day for the patient
+            double totalEE = 0.0;
+            int totalDays = 0;
+            for (ICData_Cl icData : icDataList) {
+                totalEE += icData.getEE();
+                totalDays++;
             }
-        }
 
-        return filteredGroup;
+            double eeValuePerDay = totalEE / totalDays;
+            System.out.println("Patient " + patient.getPatientCardId() + ": EE value per day = " + eeValuePerDay);
+        }
     }
 
-    private double calculateAverageEE(List<ICData_Cl> filteredEE) {
-        double totalEE = 0.0;
-        int totalDays = 0;
+    public static void main(String[] args) {
+        int patientCardId = 93; // Replace with the desired patientCardId
 
-        for (ICData_Cl icData : filteredEE) {
-            totalEE += icData.getEE();
-            totalDays++;
+        ComparableGroup comparableGroup = new ComparableGroup(patientCardId);
+        List<Patient_Cl> similarPatients = comparableGroup.getSimilarPatients();
+
+        // Process the similarPatients list as needed
+        for (Patient_Cl patient : similarPatients) {
+            System.out.println(patient); // Print or perform operations on each similar patient
         }
 
-        return totalEE / totalDays;
-    }
-
-    private double calculateDeviationPercentage(double patientEE, double averageEE) {
-        return (averageEE - patientEE) / patientEE;
+        comparableGroup.calculateEEValuePerDay();
     }
 }
-
-
